286b9450e1d658dae76247c8aace1266e666fadf
Carlos Marigorda <carlos@reevolution.com.mx>
2018-07-15 18:37:01 -0500

    first commit

---
+++ formulario/formulario.js
@@ -0,0 +1,9 @@
+// function capturar(){
+//   function Persona(nombre, edad){
+//     this.nombre = nombre;
+//     this.edad = edad;
+//   }
+
+//   var nombreCapturar = document.getElementById("nombre").value;
+//   console.log(nombreCapturar);
+// }
